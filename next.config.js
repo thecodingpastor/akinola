f07ed1910e10f05b902bcb368a0a6984587d0cfa
Michael Ayeni <thecodingpastor@gmail.com>
2022-03-31 08:11:41 +0100

    removed next.config.js from gitignore

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // reactStrictMode: true,
+  env: {
+    APP_URL: "http://localhost:4000/api/v1",
+    TOKEN_EXPIRES: "1209600000",
+    EMAIL_SERVICE_ID: "service_uqd1x8q",
+    EMAIL_TEMPLATE_ID: "template_0rxzfgg",
+    EMAIL_USER_ID: "s1kHASytRypomu8RH",
+    ADMIN_EMAIL: "iammichaelakinola@gmail.com",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
