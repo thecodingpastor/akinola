eb2ebcdbb72f3a72fdf03c6ea18f8d47d5e4e1a6
Michael Ayeni <thecodingpastor@gmail.com>
2022-04-01 17:28:07 +0100

    added Mike's changes

--- components/Pages/Index/About.jsx
+++ components/Pages/Index/About.jsx
@@ -43,24 +43,14 @@ const About = () => {
       <section className={classes.Skills}>
         <h4>Skills Summary</h4>
         <div className={classes.SkillList}>
+          <span>R</span>
+          <span>Python</span>
           <span>Data&nbsp;Science</span>
+          <span>Data&nbsp;Analysis</span>
           <span>Machine&nbsp;Learning</span>
           <span>Team&nbsp;Leadership&nbsp;&&nbsp;Development </span>
         </div>
       </section>
-      <div className={classes.Wave}>
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-            className={classes.ShapeFill}
-          ></path>
-        </svg>
-      </div>
     </div>
   );
 };
