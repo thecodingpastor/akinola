bfa90553edd47f06242135d15bf3203dccc0a538
Michael Ayeni <thecodingpastor@gmail.com>
2022-04-01 21:22:52 +0100

    done

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ module.exports = (phase) => {
       env: {
         APP_URL: "http://localhost:4000/api/v1",
         TOKEN_EXPIRES: "2592000000",
+        EMAIL_SERVICE_ID: "service_uqd1x8q",
+        EMAIL_TEMPLATE_ID: "template_0rxzfgg",
+        EMAIL_USER_ID: "s1kHASytRypomu8RH",
+        ADMIN_EMAIL: [
+          "iammichaelakinola@gmail.com",
+          "thecodingpastor@gmail.com",
+        ],
       },
       images: {
         domains: ["res.cloudinary.com"],
