b3865583e3780bb09016b83a97f5d1eb5d5b6989
Ayeni Michael <ayenimike80@gmail.com>
2022-11-04 00:33:18 +0100

    Corrected email_service_id in next.config

--- next.config.js
+++ next.config.js
@@ -9,10 +9,6 @@ module.exports = (phase) => {
         EMAIL_SERVICE_ID: "service_a0mbihc",
         EMAIL_TEMPLATE_ID: "template_0rxzfgg",
         EMAIL_USER_ID: "s1kHASytRypomu8RH",
-        ADMIN_EMAIL: [
-          "iammichaelakinola@gmail.com",
-          "thecodingpastor@gmail.com",
-        ],
       },
       images: {
         domains: ["res.cloudinary.com"],
@@ -27,10 +23,9 @@ module.exports = (phase) => {
     env: {
       APP_URL: "https://stormy-wildwood-15445.herokuapp.com/api/v1",
       TOKEN_EXPIRES: "2592000000",
-      EMAIL_SERVICE_ID: "service_uqd1x8q",
+      EMAIL_SERVICE_ID: "service_a0mbihc",
       EMAIL_TEMPLATE_ID: "template_0rxzfgg",
       EMAIL_USER_ID: "s1kHASytRypomu8RH",
-      ADMIN_EMAIL: ["iammichaelakinola@gmail.com", "thecodingpastor@gmail.com"],
     },
     images: {
       domains: ["res.cloudinary.com"],
